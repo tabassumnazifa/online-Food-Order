0c9304624f7e53451ccc3072dbcb929be03f14bd
Nazifa Tabassum <tnazifa003@gmail.com>
2026-07-14 12:20:37 +0600

    new

--- FoodDelivery.API/obj/Debug/net9.0/FoodDelivery.API.AssemblyInfo.cs
+++ FoodDelivery.API/obj/Debug/net9.0/FoodDelivery.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDelivery.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda718f090e6c2059eab32943ba29dcd99ffbdc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c945a3e6311078e6a91fbb77d207f32e2c8cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDelivery.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDelivery.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
