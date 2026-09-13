1b1e212c732470ed2bd411cee9b29e3148768d94
Nazifa Tabassum <tnazifa003@gmail.com>
2026-09-13 19:51:28 +0600

    new

--- FoodDelivery.Infrastructure/obj/Debug/net9.0/FoodDelivery.Infrastructure.AssemblyInfo.cs
+++ FoodDelivery.Infrastructure/obj/Debug/net9.0/FoodDelivery.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDelivery.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55fec88f768cb7d1276bceee98cb545b8689f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2611fde7cd012c30d9e203e2d63e6166372835")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDelivery.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDelivery.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
